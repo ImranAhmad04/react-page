2fd9956c04698f4c1698b65757c8bcd87aaca6c1
ImranAhmad04 <emranmd368@gmail.com>
2023-06-22 07:08:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const users = [
+    {
+      name:"imran ahmed",
+      age:"19",
+      phones:[
+        {home:"013...."},
+        {phones:"014..."}
+      ]
+    },
+    {
+      name:"imran hossain",
+      age:"21",
+      phones:[
+        {home:"019...."},
+        {phone:"017..."}
+      ]
+    }
+  ]
+  return (
+    <>
+      <h1>Nested Mapping</h1>
+      {users.map((user , index) => {return <div key={index}>
+        <h3> fulname:
+        {user.name}
+      </h3> ,  <p>age:{user.age}</p> 
+       {user.phones.map(( phone , index ) => <p key={index}>number:{phone.home}</p> )}  
+      </div> }) }
+    </>
+  )
+}
+
+export default App
