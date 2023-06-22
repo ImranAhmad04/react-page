af5905aefa89765b5562b66249e719424ccc62b9
ImranAhmad04 <emranmd368@gmail.com>
2023-06-22 15:45:40 +0600

    my practice

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Data from "./list.json"
 function App() {
   const [count, setCount] = useState(0)
   const users = [
@@ -17,6 +17,7 @@ function App() {
     {
       name:"imran hossain",
       age:"21",
+      
       phones:[
         {home:"019...."},
         {phone:"017..."}
